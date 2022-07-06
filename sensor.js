0c1d33d6bc274b54c8936749439ffa61cf6c63f6
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-07-07 01:04:22 +0200

    inserting car traffic on the road

--- sensor.js
+++ sensor.js
@@ -10,28 +10,43 @@ class Sensor {
         this.readings = []
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         this.#castRays();
         this.readings = [];
         for (let i = 0; i < this.rays.length; i++) {
             this.readings.push(
-                this.#getReading(this.rays[i], roadBorders)
+                this.#getReading(this.rays[i], roadBorders, traffic)
             );
         }
 
     }
 
-    #getReading(ray, roadBorders) {
+    #getReading(ray, roadBorders, traffic) {
         let touches = [];
         for (let i = 0; i < roadBorders.length; i++) {
             const touch = getIntersection(
                 ray[0],
                 ray[1],
                 roadBorders[i][0],
-                roadBorders[i][1],
+                roadBorders[i][1]
             );
             if (touch) {
                 touches.push(touch);
+            }
+        }
+
+        for (let i = 0; i < traffic.length; i++) {
+            const poly = traffic[i].polygon;
+            for (let j = 0; j < poly.length; j++) {
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j + 1) % poly.length]
+                );
+                if (value) {
+                    touches.push(value);
+                }
             }
         }
 
